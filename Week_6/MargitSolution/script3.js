ad2fe5e16ac34ccf756ab6524c196c7ef23e85bf
Huttunenbe <beniittahuttunen@gmail.com>
2025-02-25 14:58:17 +0200

    add script 3

--- Week_6/MargitSolution/script3.js
+++ Week_6/MargitSolution/script3.js
@@ -1,59 +1,70 @@
-//const animalSimple = ["pig", "cat", "coyote", "horse", "fish", "guinea pig"];
-const animalList = document.querySelector('#animalList');
-const animalButton = document.querySelector('#addAnimal')
-const nameInput = document.querySelector('#newAnimalName')
-const searchInput = document.querySelector('#searchAnimal')
-const sortButton = document.querySelector('#sortAnimals')
+const animalSimple = ["pig", "cat", "coyote", "horse", "fish", "guinea pig"];
+const animalList = document.querySelector("#animalList");
+const animalButton = document.querySelector("#addAnimal");
+const nameInput = document.querySelector("#newAnimalName");
+const searchInput = document.querySelector("#searchAnimal");
+const sortButton = document.querySelector("#sortAnimals");
+const filterType = document.querySelector("#filterType");
 
-
-  const animals = [
-    { name: 'Fox', type: 'Mammal', age: 10, color: 'White' },
-    { name: 'Elephant', type: 'Mammal', age: 25, color: 'Grey' },
-    { name: 'Eagle', type: 'Bird', age: 5, color: 'Brown' },
-    { name: 'Shark', type: 'Fish', age: 15, color: 'Grey' },
-    { name: 'Tiger', type: 'Mammal', age: 8, color: 'Orange with Black Stripes' },
-    { name: 'Parrot', type: 'Bird', age: 4, color: 'Green' },
-    { name: 'Kangaroo', type: 'Mammal', age: 12, color: 'Brown' },
-    { name: 'Penguin', type: 'Bird', age: 6, color: 'Black and White' }
-  ];
+const animals = [
+  { name: "Fox", type: "Mammal", age: 10, color: "White" },
+  { name: "Elephant", type: "Mammal", age: 25, color: "Grey" },
+  { name: "Eagle", type: "Bird", age: 5, color: "Brown" },
+  { name: "Shark", type: "Fish", age: 15, color: "Grey" },
+  { name: "Tiger", type: "Mammal", age: 8, color: "Orange with Black Stripes" },
+  { name: "Parrot", type: "Bird", age: 4, color: "Green" },
+  { name: "Kangaroo", type: "Mammal", age: 12, color: "Brown" },
+  { name: "Penguin", type: "Bird", age: 6, color: "Black and White" },
+];
 
 const displayAnimals = (animalArray) => {
-  animalList.innerHTML = ''; //will clean the ul
-for (const animal of animalArray){ //const element of object
-  const li = document.createElement('li');
-  li.textContent = `Name: ${animal.name}, Type: ${animal.type}, Age: ${animal.age}, Color: ${animal.color}`;
-  animalList.appendChild(li);
+  animalList.innerHTML = ""; //will clean the ul
+  for (const animal of animalArray) {
+    //const element of object
+    const li = document.createElement("li");
+    li.textContent = `Name: ${animal.name}, Type: ${animal.type}, Age: ${animal.age}, Color: ${animal.color}`;
+    animalList.appendChild(li);
   }
-}
+};
 
-const addAnimal = () =>{
-  const nameInput = document.querySelector('#newAnimalName').value.trim();
-  animals.push(nameInput)
-  displayAnimals(animals); 
-  console.log(animals)
-  document.querySelector('#newAnimalName').value = '';
-
-}
+const addAnimal = () => {
+  const nameInput = document.querySelector("#newAnimalName").value.trim();
+  animals.push(nameInput);
+  displayAnimals(animals);
+  console.log(animals);
+  document.querySelector("#newAnimalName").value = "";
+};
 
 const searchAnimal = () => {
   const searchText = searchInput.value.toLowerCase();
-  const filteredAnimals = animals.filter(animal => animal.name.toLowerCase().includes(searchText))
+  const filteredAnimals = animals.filter((animal) =>
+    animal.name.toLowerCase().includes(searchText)
+  );
 
-  displayAnimals(filteredAnimals)
+  displayAnimals(filteredAnimals);
+};
 
-}
+const filterByTypeAnimal = () => {
+  const selectedType = filterType.value;
 
-const sortAnimals =() =>{
-  animalSimple.sort();
-  displayAnimals(animals)
-}
+  if (selectedType === "All") {
+    displayAnimals(animals);
+  } else {
+    const filteredAnimals = animals.filter(
+      (animal) => animal.type === selectedType
+    );
+    displayAnimals(filteredAnimals);
+  }
+};
 
+const sortAnimals = () => {
+  animals.sort();
+  displayAnimals(animals);
+};
 
-animalButton.addEventListener('click', addAnimal)
-searchInput.addEventListener('input', searchAnimal)
-sortButton.addEventListener('click', sortAnimals)
+animalButton.addEventListener("click", addAnimal);
+searchInput.addEventListener("input", searchAnimal);
+sortButton.addEventListener("click", sortAnimals);
+filterType.addEventListener("change", filterByTypeAnimal);
 
-
-displayAnimals(animals)
-
-
+displayAnimals(animals);
